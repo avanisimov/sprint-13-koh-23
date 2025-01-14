aa8b17dea3ce55b3e91bfa9292670fbd548224b3
Aleksei Anisimov <anisimov.aleksei.v@gmail.com>
2025-01-14 20:05:43 +0300

    https://github.com/avanisimov/sprint-13-koh-23/issues/1 Исправлено отображение заголовка в завсимости от выбранного таба. Удален ненужный импорт

--- app/src/main/java/ru/yandex/practicum/sprint13koh23/CatalogItemViewData.kt
+++ app/src/main/java/ru/yandex/practicum/sprint13koh23/CatalogItemViewData.kt
@@ -5,7 +5,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
 import ru.yandex.practicum.sprint13koh23.databinding.VCatalogItemBinding
 
 data class CatalogItemViewData(
